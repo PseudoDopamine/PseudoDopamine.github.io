9f0b548c8e0fa182843ed997bae6f83ab5b891fb
Aaron <64151135+aaronp18@users.noreply.github.com>
2020-05-18 12:08:06 +0100

    Countdown time can be changed
    Just convert the time to UNIX time (https://www.unixtimestamp.com/index.php) and replace text in time.txt.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,8 @@
-console.log("Timer");
-var flipdown = new FlipDown(1590253200);
-flipdown.start();
+//Gets countdown time
+
+jQuery.get("/time.txt", function (data) {
+  console.log("UNIX time for event: " + data);
+  var flipdown = new FlipDown(parseInt(data));
+
+  flipdown.start();
+});
