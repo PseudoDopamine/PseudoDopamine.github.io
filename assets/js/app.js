2b259495b054d665afb658b6a07f12eeca209817
Aaron <64151135+aaronp18@users.noreply.github.com>
2020-05-18 14:11:58 +0100

    Reformatted app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,15 @@
 //Gets countdown time
+function Init() {
+  console.log("Initiating...");
+  SetCountdownTime();
+}
 
-jQuery.get("/time.txt", function (data) {
-  console.log("UNIX time for event: " + data);
-  var flipdown = new FlipDown(parseInt(data));
+function SetCountdownTime() {
+  jQuery.get("/time.txt", function (data) {
+    console.log("UNIX time for event: " + data);
+    var flipdown = new FlipDown(parseInt(data));
+    flipdown.start();
+  });
+}
 
-  flipdown.start();
-});
+Init();
