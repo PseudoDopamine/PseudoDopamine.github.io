b6ad316cb99331c8d8381a1a69da8b0ec6d3e695
Ramone Graham <ramonegraham@gmail.com>
2024-08-23 06:21:22 +0100

    Add RS7 feature
    Added The Rogue Summit 7 to the header, complete with a countdown timer and call-to-action menu.

---
+++ res/js/countdown.js
@@ -0,0 +1,26 @@
+// DOM Elements
+const timerSegments = document.querySelector(".timer").querySelectorAll("td"), streamButton = document.querySelector("header .stream.button");
+
+// State management
+const featureTime = new Date("2024-08-29T17:00:00.000+01:00").getTime();
+
+const updateTimer = setInterval(() => {
+	const timer = featureTime - new Date().getTime();
+
+	if (timer < 0) {
+		for (const segment of timerSegments) segment.innerText = 0;
+		streamButton.style.backgroundColor = "#9609F1";
+		streamButton.innerText = "Watch NOW!";
+
+		clearInterval(updateTimer);
+
+		return;
+	}
+
+	const secs = Math.floor(timer / 1000), mins = Math.floor(secs / 60), hours = Math.floor(mins / 60), days = Math.floor(hours / 24);
+
+	timerSegments[0].innerText = days;
+	timerSegments[1].innerText = hours % 24;
+	timerSegments[2].innerText = mins % 60;
+	timerSegments[3].innerText = secs % 60;
+}, 1000);
